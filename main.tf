12ed060c4a9953c425dea039cf109f2e8624240b
sujal-07-Ronny <sujalshaha974@gmail.com>
2026-01-23 14:37:13 +0530

    Fix AZ hardcoding for us-east-1

--- main.tf
+++ main.tf
@@ -10,10 +10,14 @@ resource "aws_vpc" "custom_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.custom_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-  tags = { Name = "public-subnet" }
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.custom_vpc.id
